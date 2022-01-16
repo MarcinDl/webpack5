92633914044ebc1f2fc68f28dbe9e23ceeddbcff
Marcin Dłubis <marcindl@hotmail.com>
2022-01-16 10:11:27 +0100

    2.9 Hashing

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname,"dist"),
-        filename: "bundle.js",
+        filename: "[contenthash].bundle.js",
         clean: true
     },
     devtool: "source-map",
