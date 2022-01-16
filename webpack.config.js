69d7279e3c0b62503032c3534af5466eaef31b2e
Marcin Dłubis <marcindl@hotmail.com>
2022-01-16 11:47:34 +0100

    4.1 HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");   
 
 module.exports = {
     entry: "./src/index.js",
@@ -14,6 +15,11 @@ module.exports = {
         },
         port: 9000
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
     module: {
         rules: [{
             test: /\.scss$/,
