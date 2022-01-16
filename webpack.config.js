8df596e9e125b6ab5d27abefab7c5367446d5ee0
Marcin Dłubis <marcindl@hotmail.com>
2022-01-16 12:19:41 +0100

    4.3 Autoprefixer - fix

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
             use: [
                 MiniCssExtractPlugin.loader,
                 "css-loader",
+                "sass-loader",
                 {
                     loader: "postcss-loader",
                     options: {
